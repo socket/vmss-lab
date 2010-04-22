1f0abf0bd8a469d12ea7c7f3ad49ade95b3e90a6
Alexey Streltsow <alexey@zilanetworks.com>
2010-04-22 17:17:17 +0400

    additions

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,7 +1,14 @@
+
 #include <iostream>
+#include "LogicInterpreter.h"
 
 int main (int argc, char * const argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+  //  std::cout << "Hello, World!\n";
+
+	LogicInterpreter li;
+	li.processInput("x + y + z - a");
+	li.printPostfix();
+	
+	return 0;
 }
