19c44292d32cd083eed1667f21b087844f570d5e
Alexey Streltsow <alexey@zilanetworks.com>
2010-05-09 02:42:53 +0400

    yaul specs

--- lab11/yaul_opcodes.h
+++ lab11/yaul_opcodes.h
@@ -7,3 +7,52 @@
  *
  */
 
+typedef enum { 
+	OP_PUSH_INT,
+	OP_PUSH_DOUBLE,
+	OP_PUSH_STRING,
+	
+	OP_POP,
+	OP_RETN,
+	
+	OP_ADD,
+	OP_SUB,
+	OP_MUL,
+	OP_DIV,
+	OP_BIN_AND,
+	OP_BIN_OR,
+	OP_NOT,
+	
+	OP_EQ,
+	OP_NEQ,
+	OP_LEQ,
+	OP_GEQ,
+	OP_GREATER,
+	OP_LESSER,
+	
+	OP_CMP,
+	
+	OP_JMP,
+	
+	OP_JEQ,
+	OP_JNE,
+	
+	OP_CALLFUNC,
+	OP_CALLNATIVE,
+	
+	OP_GETLOCAL,
+	OP_SETLOCAL,
+	OP_GETGLOBAL,
+	OP_SETGLOBAL,
+	
+	OP_NOP,
+	
+	OP_4BYTE_PADDING = 0xFFFFFFFF
+} yaul_opcode;
+
+typedef struct  {
+	yaul_opcode	_opcode;
+	void			 *_operand;
+} yaul_op_t;
+
+typedef yaul_op_t yaul_op;
--- lab11/yaul_vm.h
+++ lab11/yaul_vm.h
@@ -6,4 +6,64 @@
  *  Copyright 2010 Zila Networks LLC. All rights reserved.
  *
  */
+#ifndef _yaul_vm
+#define _yaul_vm
 
+#include "yaul_opcodes.h"
+
+#define YAUL_CALL_DEPTH 256;
+#define YAUL_DATA_STACK_SIZE  4096;
+
+
+typedef enum {
+	YVM_INTEGER,
+	YVM_DOUBLE,
+	YVM_STRING,
+	YVM_FUNCTION
+} yaul_type;
+
+typedef struct  {
+	const char		*_name;
+	yaul_type			 _type;
+	void					*_value;
+} yaul_var_t;
+
+typedef yaul_var_t yaul_var;
+
+typedef struct {
+	yaul_op				*_ret;
+	yaul_var			*_vars;
+} yaul_stack_t;
+
+typedef yaul_stack_t yaul_stack;
+
+typedef struct {
+	void					*_data_stack;
+	int						 _data_stack_sz;
+
+	yaul_var		  *_globals;
+	yaul_op				*_chunk;
+	
+	yaul_stack		*_call_stack;
+	int						 _call_stack_sz;
+} yaul_state_t;
+
+typedef yaul_state_t yaul_state;
+
+// YAUL API
+void				yaul_open(yaul_state **Y);
+void				yaul_close(yaul_state *Y);
+
+void				yaul_dofile(yaul_state *Y, const char *filename);
+void				yaul_dochunk(yaul_state *Y, yaul_op *code);
+
+void			  yaul_get(yaul_state *Y, yaul_var **var, int pos);
+void				yaul_pop(yaul_state *Y, int pos);
+void				yaul_push(yaul_state *Y, yaul_var *var);
+
+void				yaul_setglobal(yaul_state *Y);
+void				yaul_getglobal(yaul_state *Y);
+
+void				yaul_setcfunc(yaul_state *Y, const char *name, int (*func)(yaul_state* Y));
+
+#endif
